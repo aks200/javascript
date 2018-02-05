1cad0a263912ffe274f73bdb854905ce6f1df499
aks200 <akskn200@gmail.com>
2018-02-05 17:10:46 +0530

    js-objects

--- first.js
+++ first.js
@@ -184,3 +184,17 @@ function er(){
         msg.innerHTML="INPUT "+ err ;
     }
 }
+
+function obj()
+{
+        var person = {
+            firstName: "John",
+            lastName : "Doe",
+            id       : 5566,
+            fullName : function() {
+            return this.firstName + " " + this.lastName;
+            }
+        };
+
+        document.getElementById("demoo").innerHTML = person.fullName();
+}
